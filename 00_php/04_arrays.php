d61a6c7cc49e5b075379b368a225e15468cff87d
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-09-20 23:08:06 +0200

    Update 04_arrays.php

--- 00_php/04_arrays.php
+++ 00_php/04_arrays.php
@@ -1,7 +1,18 @@
 <?php include 'include/header.php'; ?>
 
+/**
+Usa [] cuando crees arrays manualmente, ya que es más eficiente y moderno.
+Usa array() si necesitas compatibilidad con versiones de PHP más antiguas (pre PHP 5.4).
+Usa range() para generar arrays secuenciales de manera eficiente, especialmente cuando los valores siguen un patrón o secuencia clara.
+*/
 <?php
 // Array indexado
+$arr = [1, 2, 3, "patata"];
+
+// Array con range()
+$numeros = range(1, 10);
+
+// Array con array()
 $frutas = array("manzana", "plátano", "naranja");
 echo "Primera fruta: " . $frutas[0] . "<br>"; // Salida: manzana
 
@@ -10,7 +21,7 @@ for ($i = 0; $i < count($frutas); $i++) {
     echo "Fruta $i: " . $frutas[$i] . "<br>";
 }
 
-// Array asociativo
+// Array asociativo manual (clave => valor)
 $edades = array("Ana" => 28, "Juan" => 32);
 echo "<br>La edad de Ana es: " . $edades["Ana"] . "<br>"; // Salida: 28
 
@@ -18,6 +29,14 @@ echo "<br>La edad de Ana es: " . $edades["Ana"] . "<br>"; // Salida: 28
 foreach ($edades as $nombre => $edad) {
     echo "$nombre tiene $edad años.<br>";
 }
-?>
-<?php include 'include/footer.php'; ?>
 
+// Array asociativo con array_combine()
+// ! Que ambos arrays tengan la misma longitud para evitar errores !
+$claves = ["nombre", "edad", "ciudad"];
+$valores = ["Juan", 30, "Madrid"];
+$persona = array_combine($claves, $valores);
+
+?>
+
+
+<?php include 'include/footer.php'; ?>
