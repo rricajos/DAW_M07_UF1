1832dace32c6f12c00685c5039da1cc043a1f260
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-09-21 13:16:16 +0200

    array_unique.php

--- 02_php_arrays/01_filtrar_arrays.php
+++ 02_php_arrays/01_filtrar_arrays.php
@@ -1,43 +1,37 @@
 <?php
-$numeros = [1, 2, 3];
-
-/**
- * array_rand() - Devuelve una o más claves aleatorias de un array.
- * 
- * Ejemplo: Obtener una clave aleatoria.
- */
-$claveAleatoria = array_rand($numeros);
-
-
-/**
- * array_filter() - Filtra elementos de un array usando una función de callback.
- * 
- * Ejemplo: Filtrar números pares.
- */
-$pares = array_filter($numeros, function ($n) {
-  return $n % 2 == 0;
-});
-
-/**
- * Buscar elementos
- * 
- * in_array() - Comprueba si un valor existe en un array.
- */
-$existe = in_array("banana", $frutas);
-
-/**
- * array_search() - Busca un valor y devuelve la clave correspondiente.
- */
-$clave = array_search("naranja", $frutas);
-
-
-/**
- * array_column() - Devuelve los valores de una columna específica en un array multidimensional.
- * 
- * Ejemplo: Extraer la columna "nombre" de un array asociativo.
- */
+$numeros = [1, 2, 3, 2];
 $personas = [
   ["nombre" => "Juan", "edad" => 30],
   ["nombre" => "Ana", "edad" => 25],
 ];
-$nombres = array_column($personas, "nombre");
+
+// array_unique() - Filtra valores duplicados de una array
+array_unique($numeros);
+
+
+// array_rand() - Devuelve una o más claves aleatorias de un array.
+array_rand($personas);
+
+
+// array_filter() - Filtra elementos de un array usando una función de callback.
+array_filter($numeros, function ($n) {
+  return $n % 2 == 0;
+});
+
+
+// aray_map() + array_filter() - Usa array_map() para transformar elementos y luego array_filter() para filtrar.
+array_filter(array_map('intval', $numeros), function ($value) {
+  return $value > 2; // Filtra los valores convertidos que son mayores que 2
+});
+
+
+// in_array() - Comprueba si un valor existe en un array.
+in_array(2, $numeros);
+
+
+// array_search() - Busca un valor y devuelve la clave correspondiente.
+array_search("Juan", $personas);
+
+
+// array_column() - Devuelve los valores de una columna específica en un array multidimensional.
+array_column($personas, "nombre");
--- 02_php_arrays/03_combinar_arrays.php
+++ 02_php_arrays/03_combinar_arrays.php
@@ -26,23 +26,6 @@ $claves = ["nombre", "edad"];
 $valores = ["Juan", 30];
 $persona = array_combine($claves, $valores);
 
-/**
- * array_intersect() - Devuelve los valores de un array que están presentes en todos los arrays.
- * 
- * Ejemplo: Encontrar elementos comunes entre dos arrays.
- */
-$frutas3 = ["banana", "kiwi", "uva"];
-$frutasComunes = array_intersect($frutas1, $frutas3);
-
-/**
- * array_intersect_key() - Devuelve los elementos de un array que están presentes en todos los arrays, comparando las claves.
- * 
- * Ejemplo: Encontrar elementos comunes basados en las claves.
- */
-$array1 = ["a" => 1, "b" => 2, "c" => 3];
-$array2 = ["b" => 4, "c" => 5, "d" => 6];
-$interseccionClaves = array_intersect_key($array1, $array2); 
-
 
 
 /**
